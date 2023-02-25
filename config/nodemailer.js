a598feb70d6f6fbb788645b3fd26ebe5dc606a9a
Anant Kumawat <anantkumawat22@gmail.com>
2023-02-25 19:07:37 +0530

    mail sending problem solve

--- config/nodemailer.js
+++ config/nodemailer.js
@@ -1,15 +1,15 @@
-// import nodemailer from "nodemailer";
+import nodemailer from "nodemailer";
+import dotenv from 'dotenv';
+dotenv.config();
 
-// export const transporter = nodemailer.createTransport({
-//     host: 'smtp.gmail.com',
-//     port: 465,
-//     secure: true,
-//     auth: {
-//         user: process.env.EMAIL,
-//         pass: process.env.EMAIL_PASS
-//     }
-// });
+export const transporter = nodemailer.createTransport({
+    host: 'gmail',
+    auth: {
+        user: process.env.EMAIL,
+        pass: process.env.EMAIL_PASS
+    }
+});
 
-// export const mailOptions = {
-//     from: process.env.EMAIL,
-// }
+export const mailOptions = {
+    from: process.env.EMAIL,
+}
--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,9 +1,6 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
-// import { mailOptions, transporter } from "@/config/nodemailer";
-// import sgMail from '@sendgrid/mail';
-
-import nodeMailer from "nodemailer";
+import { mailOptions, transporter } from "@/config/nodemailer";
 
 const handler = async (req, res) => {
   const data = req.body;
@@ -14,49 +11,19 @@ const handler = async (req, res) => {
   }
 
   try {
-    // await transporter.sendMail({
-    //   ...mailOptions,
-    //   to: data.email,
-    //   subject: "Markall user contact",
-    //   html: `<p>${data.message}</p>`
-    // }, (error, info) => {
-    //   if (err) {
-    //       res.status(400).json({ msg: error });
-    //       reject(err);
-    //   } else {
-    //       res.status(400).json({ msg: info });
-    //       resolve(info);
-    //   }
-    // });
-
-    // sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-    // const msg = {
-    //     to: `anantkumawat67218@gmail.com`,
-    //     from: `anantkumawat22@gmail.com`,
-    //     subject: `Markall Email`,
-    //     text: `This is test mail.`
-    // };
-    // sgMail
-    // .send(msg)
-    // .then(() => {
-    //     return res.status(200).json({ msg: 'Email Sent Successfully.' });
-    // })
-    // .catch((error) => {
-    //     return res.status(200).json({ msg: error });
-    // });
-
-    nodeMailer.transporter.sendMail({
-        from: "anantkumawat22@gmail.com",
-        to: "anantkumawat67218@gmail.com",
-        subject:'Markall',
-        html: "This is mail.",
-      
-    },(err,info)=>{
-        if(err){
-            return res.status(400).json({ msg: error });
-        }
-        return res.status(200).json({ msg: info });
-        return;
+    await transporter.sendMail({
+      ...mailOptions,
+      to: data.email,
+      subject: "Markall user contact",
+      html: `<p>${data.message}</p>`
+    }, (error, info) => {
+      if (err) {
+          res.status(400).json({ msg: error });
+          reject(err);
+      } else {
+          res.status(400).json({ msg: info });
+          resolve(info);
+      }
     });
 
     return res.status(200).json({ msg: 'Email Sent Successfully.' });
