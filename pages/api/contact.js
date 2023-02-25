32902c61839be13191a5dbfad7598c0d129f5801
Anant Kumawat <anantkumawat22@gmail.com>
2023-02-25 17:16:06 +0530

    mail sending problem solve

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -15,15 +15,7 @@ const handler = async (req, res) => {
       to: data.email,
       subject: "Markall user contact",
       html: `<p>${data.message}</p>`
-    }, (err, info) => {
-      if (err) {
-          console.error(err);
-          reject(err);
-      } else {
-          console.log(info);
-          resolve(info);
-      }}
-      );
+    });
     return res.status(200).json({ msg: 'Email Sent Successfully.' });
   } catch (error) {
     return res.status(400).json({ error: data, msg: error.message });
